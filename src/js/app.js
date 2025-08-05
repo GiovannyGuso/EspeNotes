0fee2689ef76338d448bcbd068ba650266011288
GiovannyGuso <gfduran@espe.edu.ec>
2025-08-05 11:06:19 -0500

    configuracion de boton instalar y captura de instalacion

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
-
+let deferredPrompt;
 // Funciones
 const showPostModal = () => {
   MAIN.style.display = "none"; // Oculta el contenido principal
@@ -18,14 +18,42 @@ const closePostModal = () => {
   MODAL_POST.style.transform = "translateY(100vh)"; // Oculta el modal con animación
 };
 
-// Cuando se carga el DOM
-window.addEventListener("load", () => {
-  MAIN = document.querySelector("#main");
-  MODAL_POST = document.querySelector("#modal-post-section");
-  BTN_SHOW_POST = document.querySelector("#btn-upload-post");
-  BTN_SHOW_POST.addEventListener("click", showPostModal); // Asigna el evento al botón de mostrar post
-  BTN_CANCEL_POST = document.querySelector("#btn-post-cancel");
-  BTN_CANCEL_POST.addEventListener("click", closePostModal); // Asigna el evento al botón de cerrar post
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("Evento por defecto anulado");
+  e.preventDefault(); // Previene el comportamiento por defecto
+  deferredPrompt = e; // Guarda el evento para usarlo más tarde
 });
 
+//CUANDO SE CARGA EL DOM
+window.addEventListener("load", () => {
+    MAIN = document.querySelector("#main");
+    MODAL_POST = document.querySelector("#modal-post-section");
+
+    // ✅ Apuntar correctamente al botón con el ícono "+"
+    BTN_UPLOAD_POST = document.querySelector("#btn-upload-post");
+    BTN_UPLOAD_POST.addEventListener("click", showPostModal);
+
+    // ✅ Cerrar el modal al hacer clic en "Cancelar"
+    BTN_CLOSE_POST = document.querySelector("#btn-post-cancel");
+    BTN_CLOSE_POST.addEventListener("click", closePostModal);
+
+    if(navigator.serviceWorker) {
+        const res = navigator.serviceWorker.register("/sw.js");
+        if (res) {
+            console.log("Service Worker registrado correctamente");
+        }
+    }
+    const bannerInstall = document.querySelector("#banner--install");
+    bannerInstall.addEventListener("click", async () => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt(); // Muestra el prompt de instalación
+            const response = await deferredPrompt.userChoice; // Espera la elección del usuario
+            if (response.outcome === 'accepted') {
+                console.log('Usuario aceptó la instalación de la PWA');
+            } else {
+                console.log('Usuario rechazó la instalación de la PWA');
+            }
+        }
+});
+});
 
